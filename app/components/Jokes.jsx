05bc794cc80978af773aa64e9aadb1bec502f6fc
Elizabeth Funk <elizabeth.funk@priceline.com>
2017-09-16 19:42:22 -0400

    added material-ui components and minimal styling

--- app/components/Jokes.jsx
+++ app/components/Jokes.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider.js';
+import getMuiTheme from 'material-ui/styles/getMuiTheme.js';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme.js'
+import {GridList, GridTile} from 'material-ui/GridList';
 import Iframe from 'react-iframe';
 import UserInterface from './UserInterface.jsx'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
+  },
+  gridList: {
+    width: 800,
+    height: 900,
+    overflowY: 'auto',
+  },
+};
 
 export default class BonesJokes extends Component {
   constructor (props) {
@@ -18,21 +35,28 @@ export default class BonesJokes extends Component {
     if (!this.state) { return null }
 
     return (
-      <div>
-        <Iframe
-          url="https://www.yelp.com/biz/avail-nyc-new-york"
-          width="450px"
-          height="450px"
-          display="initial"
-          position="relative"
-          allowFullScreen
-        />
-        <div>
-          <h2>Avail NYC</h2>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div style={styles.root}>
+          <GridList>
+            <GridTile>
+              <Iframe
+                url="https://www.yelp.com/biz/avail-nyc-new-york"
+                width="450px"
+                height="450px"
+                display="initial"
+                position="relative"
+                allowFullScreen
+              />
+              <div>
+                <h2>Avail NYC</h2>
+              </div>
+            </GridTile>
+            <GridTile>
+             <UserInterface></UserInterface>
+            </GridTile>
+         </GridList>
         </div>
-
-        <UserInterface></UserInterface>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
